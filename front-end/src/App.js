a73297c794b086feda0cc1393efb6efa59514015
piercecollazo <piercecollazo@gmail.com>
2019-09-17 15:33:26 -0400

    added basic foundation for front-end with components and google maps API, as well as dependencies. No real visuals yet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Route, Link, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Nav from './components/Nav'
+import Home from './components/Home'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+        </div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+    )
+  }
 }
+
 
 export default App;
