fa79f38c14a7dc69f9b6c5246f5429be31187e17
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-09-19 14:23:08 -0400

    Updated React routing and creating Main page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react'
-import {Route, Link, BrowserRouter as Router, Switch, Redirect} from 
-'react-router-dom'
 import './App.css'
-
 import Nav from './components/Nav'
-import Home from './components/Home'
-import Profile from './components/Profile'
-import Event from './components/EventPage'
-import About from './components/About'
-import NotFound from './components/NotFound'
+import Main from './components/Main'
 
-export default class App extends Component {
-  render() {
+function App() {
     return (
-      <Router>
-        <div>
+         <div className="App">
           <Nav />
+
+          <Main />
+
         </div>
-        <br/>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/profile' component={Profile} />
-          <Route path='/About' component={About} />
-          <Route path='/Event' component={Event} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
     )
-  }
 }
-
-
-
+        
+export default App;
---
+++ front-end/src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from "./Home";
+import Profile from "./Profile";
+import About from "./About";
+import Event from "./EventPage"
+import NotFound from "./NotFound";
+
+const Main = () => (
+    <main>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/profile' component={Profile} />
+        <Route path='/About' component={About}/>
+        <Route path='/Event' component={Event}/>
+        <Route component={NotFound} />
+      </Switch>
+    </main>
+  )
+  
+  export default Main
