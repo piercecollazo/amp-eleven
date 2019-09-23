137869c80d62762ab1087bc91f5c894de222c711
piercecollazo <piercecollazo@gmail.com>
2019-09-23 15:40:18 -0400

    Added API support and much of the sign in functionallity, still some functions needed

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+
 import BackgroundVideo from '../Assets/video/Amp.mp4'
 
-const Home = () => (
-    <div>
-            <header className="masthead">
-                <div className="container d-flex h-100 align-items-center">
-                  <div className="mx-auto text-center">
-                    <h1 className="mx-auto my-0 text-uppercase">Grayscale</h1>
-                    <h2 className="text-black-50 mx-auto mt-2 mb-5">A free, responsive, one             page Bootstrap theme created by Start Bootstrap.</h2>
-                    <a href="#about" className="btn btn-primary js-scroll-trigger">Get          Started</a>
-                  </div>
-                </div>
-                {/* <video autoplay muted loop id="backgroundVideo">    
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <video autoplay='autoplay' muted='muted' loop='loop' id="backgroundVideo">    
                     <source src={BackgroundVideo} type="video/mp4" />
-                </video> */}
-            </header>
-        </div>
+                </video>
+                    <header className="masthead">
+                        <div className="container d-flex h-100 align-items-center">
+                          <div className="mx-auto text-center">
+                            <h1 className="mx-auto my-0 text-uppercase">Amp-Eleven</h1>
+                            <h2 className="text-black-50 mx-auto mt-2 mb-5">Welcome to Amp-Eleven. From the undergroundto the          stage</h2>
+                            <a href="#about" className="btn btn-primary js-scroll-trigger">Get          Started</a>
+                          </div>
+                        </div>
+                    </header>
+            </div>
         )
+    }
+}
 
-export default Home
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,39 +1,98 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link, Route, BrowserRouter as Router, Switch, } from 'react-router-dom'
+import Context from '../context/Context'
 
-import { Link } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Event from './EventPage'
+import Profile from './Profile'
+import NotFound from './NotFound'
 
-const Navigation = () => (
-   
-                <nav className="navbar navbar-expand-lg bg-secondary text-uppercase     fixed-top" id="mainNav">
-                    <div className="container">
-                      
-                      <Link to="/">Home</Link>
-                      
-                      <button className="navbar-toggler navbar-toggler-right        text-uppercase font-weight-bold bg-primary text-white   rounded" type="button" data-toggle="collapse"        data-target="#navbarResponsive"     aria-controls="navbarResponsive" aria-expanded="false"        aria-label="Toggle navigation">
-                        Menu
-                        <i className="fas fa-bars"></i>
-                      </button>
+export default class Nav extends Component {
+    static contextType = Context
 
-                      <div className="collapse navbar-collapse"     id="navbarResponsive">
-                        <ul className="navbar-nav ml-auto">
+    constructor(props){
+        super(props)
 
-                        <li className="nav-item mx-0 mx-lg-1">
-                            <Link to="/About">About</Link>
-                          </li>
+        this.state={
+            email: '',
+            pass: '',
+            remember: false
+        }
+    }
+
+    
+
+    render (){
+        const { isAuth, user, logout, signIn } = this.context
+
+        return (
+                <Router>
+                    <nav className="navbar navbar-expand-lg bg-secondary text-uppercase     fixed-top" id="mainNav">
+                        <div className="container">
                           
-                          <li className="nav-item mx-0 mx-lg-1">
-                            <Link to="/Profile">Profile</Link>
-                          </li>
+                          <Link to="/">Home</Link>
                           
-                          <li className="nav-item mx-0 mx-lg-1">
-                            <Link to="/Event">Event</Link>
-                          </li>
-                          
-                        </ul>
-                      </div>
-                    </div>
-                </nav>
+                          <button className="navbar-toggler navbar-toggler-right        text-uppercase font-weight-bold bg-primary text-white   rounded" type="button" data-toggle="collapse"        data-target="#navbarResponsive"     aria-controls="navbarResponsive" aria-expanded="false"        aria-label="Toggle navigation">
+                            Menu
+                            <i className="fas fa-bars"></i>
+                          </button>
+    
+                          <div className="collapse navbar-collapse"     id="navbarResponsive">
+                            <ul className="navbar-nav ml-auto">
+    
+                            <li className="nav-item mx-0 mx-lg-1">
+                                <Link to="/About">About</Link>
+                              </li>
+                              
+                              <li className="nav-item mx-0 mx-lg-1">
+                                <Link to="/Profile">Profile</Link>
+                              </li>
+                              
+                              <li className="nav-item mx-0 mx-lg-1">
+                                <Link to="/Event">Event</Link>
+                              </li>
+                              
+                              <li className="nav-item mx-0 mx-lg-1">
+                                <i className="material-icons" data-toggle='dropdown'>person</i>
+                                <div className="dropdown-menu justify-content-end">
+                                    <form className="px-4 py-3" onSubmit={()=>{signIn()}}>
+                                      <div className="form-group">
+                                        <label for="exampleDropdownFormEmail1">Email address</label>
+                                        <input type="email" class="form-control"                            id="exampleDropdownFormEmail1" placeholder="email@example.com" />
+                                      </div>
+                                      <div className="form-group">
+                                        <label for="exampleDropdownFormPassword1">Password</label>
+                                        <input type="password" class="form-control"                             id="exampleDropdownFormPassword1" placeholder="Password" />
+                                      </div>
+                                      <div className="form-check">
+                                        <input type="checkbox" class="form-check-input" id="dropdownCheck" />
+                                        <label className="form-check-label" for="dropdownCheck">
+                                          Remember me
+                                        </label>
+                                      </div>
+                                      <button type="submit" class="btn btn-primary">Sign in</button>
+                                    </form>
+                                      <div className="dropdown-divider"></div>
+                                      <Link className="dropdown-item" to="/">New around here? Sign up</Link>
+                                      <Link className="dropdown-item" to="/">Forgot password?</Link>
+                                    </div>
+                              </li>
+                              
+                            </ul>
+                          </div>
+                        </div>
+                    </nav>
+    
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/profile' component={Profile} />
+                        <Route path='/About' component={About} />
+                        <Route path='/Event' component={Event} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Router>
+        )
+    }
+}
             
-)
-
-export default Navigation
