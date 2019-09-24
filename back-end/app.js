5d9d19ac46873888388766f28f3c8ff35411bc7b
piercecollazo <piercecollazo@gmail.com>
2019-09-24 14:47:02 -0400

    Fixed sign in cors policy issues

--- back-end/app.js
+++ back-end/app.js
@@ -5,6 +5,7 @@ var cookieParser    = require('cookie-parser');
 var logger          = require('morgan');
 var mongoose        = require('mongoose');
 var passport        = require('passport')
+var cors            = require('cors')
 
 var indexRouter     = require('./routes/index');
 var usersRouter     = require('./routes/users/users');
@@ -40,6 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
